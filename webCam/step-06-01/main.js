c009665787e26fb1c49e0ddd70e85507e4934c80
you <20211939@edu.hanbat.ac.kr>
2024-10-01 19:34:27 +0900

    tt

--- webCam/step-06-01/main.js
+++ webCam/step-06-01/main.js
@@ -1,77 +1,56 @@
-//npm install -g node-pre-gyp(wrtc안깔리는 문제)
-//npm install wrtc
-
 'use strict';
 
-const wrtc = require('wrtc');
-const io = require('socket.io-client');
+const wrtc = require('wrtc');  // WebRTC 패키지
+const io = require('socket.io-client');  // Socket.IO 클라이언트
 
 // WebRTC 관련 변수
 let isChannelReady = false;
 let isInitiator = false;
 let isStarted = false;
-let localStream;
 let pc;
 let dataChannel;
 
 const pcConfig = {
   'iceServers': [
-    {'urls': 'stun:stun.l.google.com:19302'}
-    // TURN 서버는 필요시 추가
+    { 'urls': 'stun:stun.l.google.com:19302' }  // STUN 서버 사용
   ]
 };
 
-// SDP Constraints
-const sdpConstraints = {
-  offerToReceiveAudio: false,
-  offerToReceiveVideo: false
-};
-
-/////////////////////////////////////////////
-
-const room = 'foo'; // 고정된 방 이름
-
 // Signaling 서버에 연결
-const socket = io.connect('http://192.168.50.140:3000'); // signaling 서버 주소
-// var socket = io.connect();
+const socket = io.connect('http://192.168.219.102:3000');  // Socket.IO 서버 주소 (필요에 맞게 변경)
+
+socket.on('connect', () => {
+  console.log('Connected to server, socket id:', socket.id);
+});
+
+const room = 'foo';  // 고정된 방 이름
 
 if (room !== '') {
   socket.emit('create or join', room);
   console.log('Attempted to create or join room', room);
 }
 
-socket.on('created', function (room) {
+socket.on('created', function(room) {
   console.log('Created room ' + room);
   isInitiator = true;
 });
 
-socket.on('full', function (room) {
+socket.on('full', function(room) {
   console.log('Room ' + room + ' is full');
 });
 
-socket.on('join', function (room) {
+socket.on('join', function(room) {
   console.log('Another peer made a request to join room ' + room);
   isChannelReady = true;
 });
 
-socket.on('joined', function (room) {
-  console.log('joined: ' + room);
+socket.on('joined', function(room) {
+  console.log('Joined: ' + room);
   isChannelReady = true;
 });
 
-socket.on('log', function (array) {
-  console.log.apply(console, array);
-});
-
-////////////////////////////////////////////////
-
-function sendMessage(message) {
-  console.log('Client sending message: ', message);
-  socket.emit('message', message);
-}
-
 // Signaling 메시지 수신
-socket.on('message', async function (message) {
+socket.on('message', async function(message) {
   console.log('Client received message:', message);
   if (message.type === 'offer') {
     if (!isInitiator && !isStarted) {
@@ -94,8 +73,8 @@ socket.on('message', async function (message) {
 
 ////////////////////////////////////////////////////
 
+// DataChannel 및 PeerConnection 설정
 async function maybeStart() {
-  console.log('>>>>>>> maybeStart() ', isStarted, isChannelReady);
   if (!isStarted && isChannelReady) {
     console.log('>>>>>> creating peer connection');
     createPeerConnection();
@@ -110,64 +89,54 @@ function createPeerConnection() {
   try {
     pc = new wrtc.RTCPeerConnection(pcConfig);
     pc.onicecandidate = handleIceCandidate;
-    pc.ondatachannel = receiveChannelCallback; // DataChannel 수신 설정
-    console.log('Created RTCPeerConnnection');
+    pc.ondatachannel = receiveChannelCallback;  // DataChannel 수신 설정
+    console.log('Created RTCPeerConnection');
   } catch (e) {
-    console.log('Failed to create PeerConnection, exception: ' + e.message);
+    console.log('Failed to create PeerConnection:', e.message);
     return;
   }
 }
 
 // ICE 후보 정보 전송
 function handleIceCandidate(event) {
-  console.log('icecandidate event: ', event);
   if (event.candidate) {
-    console.log('Sending ICE candidate');
-    sendMessage({
+    socket.emit('message', {
       type: 'candidate',
       label: event.candidate.sdpMLineIndex,
       id: event.candidate.sdpMid,
       candidate: event.candidate.candidate
     });
-  } else {
-    console.log('End of candidates.');
   }
-  
 }
 
+// Offer 전송
 async function doCall() {
-  console.log('Sending offer to peer');
-  dataChannel = pc.createDataChannel('chat'); // DataChannel 생성
+  console.log('Creating offer');
+  dataChannel = pc.createDataChannel('chat');  // DataChannel 생성
   setupDataChannel();
 
-  try {
-    const offer = await pc.createOffer(sdpConstraints);
-    console.log('Offer created:', offer);
-    await setLocalAndSendMessage(offer);
-  } catch (error) {
-    console.error('Error during offer creation:', error);
-  }
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  socket.emit('message', offer);
 }
 
-
+// Answer 전송
 async function doAnswer() {
-  console.log('Sending answer to peer.');
+  console.log('Sending answer');
   const answer = await pc.createAnswer();
-  await setLocalAndSendMessage(answer);
+  await pc.setLocalDescription(answer);
+  socket.emit('message', answer);
 }
 
-async function setLocalAndSendMessage(sessionDescription) {
-  await pc.setLocalDescription(sessionDescription);
-  sendMessage(sessionDescription);
-}
-
+// DataChannel 설정
 function setupDataChannel() {
-  dataChannel.onopen = function () {
+  dataChannel.onopen = () => {
     console.log('Data channel is open');
+    dataChannel.send('Hello from ' + (isInitiator ? 'Initiator' : 'Receiver'));
   };
 
-  dataChannel.onmessage = function (event) {
-    console.log('Received message: ', event.data);
+  dataChannel.onmessage = (event) => {
+    console.log('Received message:', event.data);
   };
 }
 
